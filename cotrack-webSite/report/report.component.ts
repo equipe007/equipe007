f3dd9190eff5e8aceb2b20e6ee64062c16753cef
Andre Andrade <72312992+Mr-Andor@users.noreply.github.com>
2020-10-04 06:43:58 -0300

    Add files via upload

---
+++ cotrack-webSite/report/report.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-report',
+  templateUrl: './report.component.html',
+  styleUrls: ['./report.component.css']
+})
+export class ReportComponent implements OnInit {
+
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
